95e081ae9726a1ac78f7eef6e4e7777f9b85040b
Irene Rosell <irenerosell@mbp-de-irene.home>
2020-06-22 21:18:53 +0200

    Implement component in the app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Game v-if="tries < 5" />
+    <EndGame v-else />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from "./components/Game.vue";
+import EndGame from "./components/EndGame.vue";
+import { mapState } from "vuex";
 
 export default {
-  name: 'App',
+  name: "App",
+  computed: {
+    ...mapState(["tries"])
+  },
   components: {
-    HelloWorld
+    Game,
+    EndGame
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
